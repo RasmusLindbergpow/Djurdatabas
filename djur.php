cb7c673aef6445e5c43d56bff63675bc72ba358e
RasmusLindbergpow <rasmus.lindberg@elev.ga.ntig.se>
2024-09-17 14:57:20 +0200

    kod till djur

---
+++ djur.php
@@ -0,0 +1,14 @@
+<?php
+
+class Djur {
+    protected $name
+    protected $sound
+    protected $age
+    function __construct($name, $sound, $age) {
+        $this->name = $name;
+        $this->sound = $sound;
+        $this->age = $age;
+    }
+}
+
+?>
