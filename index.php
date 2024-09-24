a508550e161eb6fa34ab642b7d389cc8651fb00e
gabriel1-ux <gabriel.ryberg@elev.ga.ntig.se>
2024-09-24 15:18:19 +0200

    css

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8"> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Djur</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Djur</title>   
+    <link rel="stylesheet" href="style.css"> 
 </head>
 
 <body>
