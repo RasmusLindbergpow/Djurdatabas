be3e0af0723e356ff440dc47eb2f2480b15accfa
Asghar1111 <aliasghar.ahadi@elev.ga.ntig.se>
2024-09-24 13:59:51 +0200

    css

---
+++ hund.php
@@ -0,0 +1,18 @@
+<?php
+class hund {
+    protected $name;
+    protected $color;
+
+
+    function set_name( $name) {
+        $this->name = $name;
+    }
+
+        function get_name() {
+            return $this->name;
+          }
+        }
+        ?>
+
+
+    
